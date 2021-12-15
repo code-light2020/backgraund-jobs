899c2f7c30c296e416a531384a03cd2cc852aa48
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2021-12-15 23:28:33 +0100

    create post route and mailtrap redis env add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get("/", (re: Request, res: Response) =>
-  res.json({ message: "ola mundo" })
+app.get('/', (re: Request, res: Response) =>
+  res.json({ message: 'ola mundo' }),
 );
 
-app.listen(3000, () => console.log("app runnig o port 3000"));
+app.post('/register', async (req: Request, res: Response) => {
+  const { name, message, email } = req.body;
+  return res.status(200).json({ message: 'User created' });
+});
+
+// eslint-disable-next-line no-console
+app.listen(3000, () => console.log('app runnig o port 3000'));
